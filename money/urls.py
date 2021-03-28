0fb3e4e45389de8fcfe3155c3a78a49d0611327d
prabodh44 <prabodhtuladhar44@gmail.com>
2021-03-28 21:12:03 +0545

    Prabodh: changed the first screen from Dashboard to login | changed the LOGIN_REDIRECT_URL to /

--- money/urls.py
+++ money/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name='index'),
+    path('dashboard', views.index_view, name='index'),
     path('addTransaction', views.addTransaction_view, name="addTransaction"),
     path('transactions', views.transactions_view, name="transactions"),
     path('transactionTypes',views.transactionTypes_view, name="transactionTypes"),
-    path('login',views.login_view, name="login"),
+    path('',views.login_view, name="login"),
     path('register',views.register_view, name="register"),
     path('logout', views.logout_view, name="logout"),
     path('pie-chart',views.pie_chart_view, name="pieChart"),
